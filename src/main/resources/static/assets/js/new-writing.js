c38de479c02ddf4b42ae2d5d1cee64048359734e
mirangee <pmrang@live.co.kr>
2024-04-17 16:47:19 +0900

    캐시 삭제 후 변경사항 커밋

--- src/main/resources/static/assets/js/new-writing.js
+++
@@ -1,9 +0,0 @@
-const imageTag = document.getElementById("ex_file");
-imageTag.addEventListener('change', function () {
-    console.log('파일선택');
-    while (onnode.hasChildNodes()) {
-        onnode.removeChild(onnode.firstChild);
-    }
-    loadImg(this);
-
-});
